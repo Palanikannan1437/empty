4192485a3a0a4dd9a1e029305ef9d4e25c3c77bd
PalanikannanM <palanikannan.m2020@vitstudent.ac.in>
2023-05-25 13:41:02 +0000

    added middle step

--- packages/demo/core/postinstall/main.go
+++ packages/demo/core/postinstall/main.go
@@ -5,17 +5,27 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"runtime"
 )
 
 func getSchemaPath() string {
-	schemaPath := filepath.Join("dist", "schema.json")
-	println(schemaPath)
+	currentPath := absPath()
+	schemaPath := filepath.Join(currentPath, "..", "..", "dist", "schema.json")
 	return schemaPath
 }
 
-func copySchema(schemaPath string) error {
-	targetPath := filepath.Join("..", "..", "cms", "src", "api", "separator2", "content-types", "separator2", "schema.json")
+func copySchema(schemaPath string, collection string) error {
+	fmt.Print("Enter the path of your CMS directory: ")
 
+	//Declare variable to store input
+	var cmsDir string
+
+	//Take input from user
+	fmt.Scanln(&cmsDir)
+
+	targetPath := filepath.Join(cmsDir, "src", "api", collection, "content-types", collection, "schema.json")
+
+	fmt.Println(targetPath)
 	data, err := ioutil.ReadFile(schemaPath)
 	if err != nil {
 		return err
@@ -34,10 +44,18 @@ func copySchema(schemaPath string) error {
 	return nil
 }
 
+func absPath() string {
+	_, currentFilePath, _, _ := runtime.Caller(0)
+	currentDir := filepath.Dir(currentFilePath)
+	return currentDir
+}
+
 func main() {
+	absPath()
+
 	schemaPath := getSchemaPath()
 
-	if err := copySchema(schemaPath); err != nil {
+	if err := copySchema(schemaPath, "seperator2"); err != nil {
 		fmt.Println("Error copying schema:", err)
 		os.Exit(1)
 	}
---
+++ packages/demo/core/postuninstall/go.mod
@@ -0,0 +1,3 @@
+module postuninstall
+
+go 1.19
